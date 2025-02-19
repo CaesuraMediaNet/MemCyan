95b37eb843734ca7d1b80998c190f8ede2dff210
Caesura Media Net <andyc@caesuramedia.net>
2025-02-19 12:46:03 +0000

    REmoved Google Play link

--- pages/index.js
+++ pages/index.js
@@ -569,11 +569,6 @@ export default function Game () {
                      Copyright &copy; Caesura Media Limited,  {new Date().getFullYear()}. All Rights Reserved.
                   </Col>
                   <Col>
-                     <div className={styles.googlePlayIcon}>
-                        {showGooglePlay && <a target="_blank" href='https://play.google.com/store/apps/details?id=com.memcyan&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>}
-                     </div>
-                  </Col>
-                  <Col>
                      <a
                         href="/privacy_policy/index.html"
                         title={"Privacy Policy"}
